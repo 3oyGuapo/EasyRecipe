acf6e3850e9ddd8747e8b7c7248b005e5dcf8aed
3oyGuapo <fdafdaq@gmail.com>
2026-01-08 15:54:05 +0000

    feat: Implement RecipeDetail component for presenting recipe information

---
+++ EasyRecipeWeb/src/components/RecipeDetail.tsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import type { Recipe } from "../types";
+
+function RecipeDetail() {
+  const { id } = useParams();
+  const [recipe, setRecipe] = useState<Recipe | null>(null);
+
+  useEffect(() => {
+    const fetchDetail = async () => {
+      try {
+        const response = await fetch(
+          `https://localhost:7287/api/Recipes/${id}`
+        );
+        const detail = await response.json();
+
+        setRecipe(detail);
+      } catch (error) {
+        console.error("Error fetching details", error);
+      }
+    };
+    fetchDetail();
+  }, [id]);
+
+  if (!recipe) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <>
+      <div>
+        <h2>Recipe Id: {id}</h2>
+        <ul>
+          {recipe.ingredientsList.map((ingredient) => (
+            <li>{ingredient.name}</li>
+          ))}
+        </ul>
+
+        <ul>
+          {recipe.stepsList.map((step) => (
+            <li>
+              {step.stepOrder} {step.stepContent}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default RecipeDetail;
