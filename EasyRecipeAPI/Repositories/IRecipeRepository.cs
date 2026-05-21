d4e45cb92bb86c45e74a4fa838ee9cf464b95048
3oyGuapo <fdafdaq@gmail.com>
2026-05-21 15:47:02 +0100

    feat: Implement IRecipeRepository and RecipeRepository class as the repository layer to communicate with database

---
+++ EasyRecipeAPI/Repositories/IRecipeRepository.cs
@@ -0,0 +1,14 @@
+﻿using RecipeData;
+
+namespace EasyRecipeAPI.Repositories
+{
+    public interface IRecipeRepository
+    {
+        Task<IEnumerable<Recipe>> GetAllRecipesAsync();
+        Task<Recipe?> GetRecipeByIdAsync(int id);
+        Task AddRecipeAsync(Recipe newRecipe);
+        Task DeleteRecipeAsync(Recipe deleteRecipe);
+        Task<Tag?> GetTagByNameAsync(string name);
+        Task SaveChangesAsync();
+    }
+}
---
+++ EasyRecipeAPI/Repositories/RecipeRepository.cs
@@ -0,0 +1,55 @@
+﻿using EasyRecipeAPI.DbContextData;
+using Microsoft.EntityFrameworkCore;
+using RecipeData;
+
+namespace EasyRecipeAPI.Repositories
+{
+    public class RecipeRepository : IRecipeRepository
+    {
+        private readonly EasyRecipeDbContext _context;
+
+        public RecipeRepository(EasyRecipeDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Recipe>> GetAllRecipesAsync()
+        {
+            return await _context.Recipes
+                .Include(recipe => recipe.IngredientsList)
+                .Include(recipe => recipe.StepsList)
+                .Include(recipe => recipe.TagsList)
+                .ToListAsync();
+        }
+
+        public async Task<Recipe?> GetRecipeByIdAsync(int id)
+        {
+            return await _context.Recipes
+                .Include(recipe => recipe.IngredientsList)
+                .Include(recipe => recipe.StepsList)
+                .Include(recipe => recipe.TagsList)
+                .FirstOrDefaultAsync(recipe => recipe.ID == id);
+        }
+
+        public async Task AddRecipeAsync(Recipe newRecipe)
+        {
+            await _context.Recipes.AddAsync(newRecipe);
+        }
+
+        public async Task DeleteRecipeAsync(Recipe deleteRecipe)
+        {
+            _context.Recipes.Remove(deleteRecipe);
+            await Task.CompletedTask;
+        }
+
+        public async Task<Tag?> GetTagByNameAsync(string name)
+        {
+            return await _context.Tags.FirstOrDefaultAsync(tag => tag.Name == name);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
