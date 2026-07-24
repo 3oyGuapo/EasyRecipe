561d5113d7d4984cf6428dab449fa1b85e803d44
3oyGuapo <fdafdaq@gmail.com>
2026-07-24 15:14:33 +0100

    test(hooks): Add MemoryRouter wrapper to fix useNavigate context error

--- EasyRecipeWeb/src/hooks/useRecipeForm.test.ts
+++ EasyRecipeWeb/src/hooks/useRecipeForm.test.ts
@@ -3,10 +3,14 @@ import { renderHook, act } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import { useRecipeForm } from "./useRecipeForm";
 
-test("Initial value should be empty string and array", () => {
-  const { result } = renderHook(() => useRecipeForm(), {
+function renderRecipeForm(id?: string) {
+  return renderHook(() => useRecipeForm(id), {
     wrapper: MemoryRouter,
   });
+}
+
+test("Initial value should be empty string and array", () => {
+  const { result } = renderRecipeForm();
 
   expect(result.current.recipeName).toBe("");
   expect(result.current.ingredients).toEqual([]);
