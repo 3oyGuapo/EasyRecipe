f87005ab5499626ca83a68f12657f2c6d81b2916
3oyGuapo <fdafdaq@gmail.com>
2026-07-27 14:40:21 +0100

    test(hooks): Add tests for addIngredient and addStep functions

--- EasyRecipeWeb/src/hooks/useRecipeForm.test.ts
+++ EasyRecipeWeb/src/hooks/useRecipeForm.test.ts
@@ -40,3 +40,36 @@ test("addIngredient should add an empty ingredient", () => {
   expect(result.current.ingredients).toHaveLength(1);
   expect(result.current.ingredients[0]).toEqual({ name: "", unitAmount: "" });
 });
+
+test("Calling addIngredient multiple times should increase array length", () => {
+  const { result } = renderRecipeForm();
+
+  act(() => {
+    result.current.addIngredient();
+  });
+  act(() => {
+    result.current.addIngredient();
+  });
+
+  expect(result.current.ingredients).toHaveLength(2);
+  expect(result.current.ingredients[0]).toEqual({ name: "", unitAmount: "" });
+  expect(result.current.ingredients[1]).toEqual({ name: "", unitAmount: "" });
+});
+
+test("addStep should calculate stepOrder based on the order", () => {
+  const { result } = renderRecipeForm();
+
+  act(() => {
+    result.current.addStep();
+  });
+  act(() => {
+    result.current.addStep();
+  });
+  act(() => {
+    result.current.addStep();
+  });
+
+  expect(result.current.steps).toHaveLength(3);
+  expect(result.current.steps[0].stepOrder).toBe(1);
+  expect(result.current.steps[2].stepOrder).toBe(3);
+});
